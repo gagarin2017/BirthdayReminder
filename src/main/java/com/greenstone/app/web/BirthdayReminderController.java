832c2bd12412b486ad7993038cabe7c0bd8c4043
Yury Gagarin <yury_gagarin2017@mail.ru>
2020-09-14 19:46:22 +0100

    added personhome

--- src/main/java/com/greenstone/app/web/BirthdayReminderController.java
+++ src/main/java/com/greenstone/app/web/BirthdayReminderController.java
@@ -5,9 +5,11 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.greenstone.domain.Person;
 import com.greenstone.services.PersonService;
@@ -20,8 +22,15 @@ public class BirthdayReminderController /* implements WebMvcConfigurer */ {
     private PersonService personService;
 	
 	@GetMapping("/")
+	public ModelAndView showHome() {
+		final ModelAndView modelAndView = new ModelAndView("personhome");
+		modelAndView.addObject("persons", personService.findAll());
+		return modelAndView;
+	}
+	
+	@GetMapping("/addPerson")
 	public String showForm(Person person) {
-		return "personhome";
+		return "personform";
 	}
 	
 	@PostMapping("/")
