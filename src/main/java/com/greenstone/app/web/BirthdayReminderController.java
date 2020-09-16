6dc9297cb8d9c3686a0a8c0472fce454ddf97c3c
Yury Gagarin <yury_gagarin2017@mail.ru>
2020-09-16 08:43:37 +0100

    Adding CRUD functionality

--- src/main/java/com/greenstone/app/web/BirthdayReminderController.java
+++ src/main/java/com/greenstone/app/web/BirthdayReminderController.java
@@ -1,14 +1,18 @@
 package com.greenstone.app.web;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.greenstone.domain.Person;
@@ -40,7 +44,24 @@ public class BirthdayReminderController /* implements WebMvcConfigurer */ {
 			return "personhome";
 		}
 
-		personService.savePerson(person);
+		personService.updatePerson(person);
 		return "personview";
 	}
+	
+	@GetMapping("/update/person")
+	public String showPerson(@RequestParam String id, Model model) {
+		Optional<Person> person = personService.findById(Integer.valueOf(id));
+	    model.addAttribute("person", person.get()); 
+//		final ModelMap modelMap = new ModelMap("person", personService.findById(Integer.valueOf(id)));
+		return "personform";
+	}
+	
+	@GetMapping("/delete/person")
+	public ModelAndView deletePerson(@RequestParam String id) {
+		personService.deletePerson(Integer.valueOf(id));
+		final ModelAndView modelAndView = new ModelAndView("personhome");
+		modelAndView.addObject("persons", personService.findAll());
+		return modelAndView;
+	}
+	
 }
