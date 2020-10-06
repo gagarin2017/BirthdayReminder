3e0d5f0f87a5f51e538f85173b720c93862f63f5
Yury Gagarin <yury_gagarin2017@mail.ru>
2020-10-06 21:27:19 +0100

    - cleaned some code - added color codes to the table. i.e. if birthday is less than a week away then the row will be colored red

--- src/main/java/com/greenstone/services/PersonServiceImpl.java
+++ src/main/java/com/greenstone/services/PersonServiceImpl.java
@@ -1,8 +1,5 @@
 package com.greenstone.services;
 
-import java.time.LocalDate;
-import java.time.Period;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -57,12 +54,6 @@ public class PersonServiceImpl implements PersonService {
 				dbPersonToUpdate.get().setDateOfBirth(formPerson.getDateOfBirth());
 				
 				birthdayService.updatePersonsBirthday(dbPersonToUpdate.get());
-				
-				//TODO: update Birthday too
-//				Optional<Person> dbPersonToUpdate = personRepository.findById(formPerson.getId());
-//				final Birthday newBirthday = generateBirthday(dbPersonToUpdate.get());
-//				dbPersonToUpdate.get().setBirthday(newBirthday);
-//				newBirthday.setPerson(formPerson);
 			}
 		}
 		
@@ -97,40 +88,6 @@ public class PersonServiceImpl implements PersonService {
 		return personsWithBirthdaysDue;
 	}
 
-	/**
-	 * Calculates the time difference in days between now and the date of birthday.
-	 * 
-	 * @param person
-	 * @return true if the number of days before birthday is less than defined in params.
-	 */
-	// TODO; fix the limit. How soon before birthday user should be reminded of the upcoming birthday.
-	private boolean isBirthdayDue(final Person person) {
-		
-		boolean isBirthdayDue = false;
-
-		if (person.getDateOfBirth() != null) {
-
-			LocalDate today = LocalDate.now();
-			LocalDate birthday = person.getDateOfBirth();
-
-			LocalDate nextBDay = birthday.withYear(today.getYear());
-
-			// If your birthday has occurred this year already, add 1 to the year.
-			if (nextBDay.isBefore(today) || nextBDay.isEqual(today)) {
-				nextBDay = nextBDay.plusYears(1);
-			}
-
-			Period p = Period.between(today, nextBDay);
-			long p2 = ChronoUnit.DAYS.between(today, nextBDay);
-			System.out.println("There are " + p.getMonths() + " months, and " + p.getDays()
-					+ " days until your next birthday. (" + p2 + " total)");
-
-			isBirthdayDue = p2 > 3;
-		}
-		
-		return isBirthdayDue;
-	}
-	
 	@Override
 	public List<Person> pullPersonsData() {
 		
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.person (id, dateofbirth, firstname, lastname) VALUES(1, '1983-01-20', 'Steven', '');
-INSERT INTO public.person (id, dateofbirth, firstname, lastname) VALUES(2, '1947-03-01', 'Linda', '');
+INSERT INTO public.person (id, dateofbirth, firstname, lastname) VALUES(1, '1983-10-15', 'Steven', '');
+INSERT INTO public.person (id, dateofbirth, firstname, lastname) VALUES(2, '1947-10-29', 'Linda', '');
 INSERT INTO public.person (id, dateofbirth, firstname, lastname) VALUES(3, '1956-07-02', 'Fiona', '');
-INSERT INTO public.person (id, dateofbirth, firstname, lastname) VALUES(4, '1985-05-09', 'Bob', '');
+INSERT INTO public.person (id, dateofbirth, firstname, lastname) VALUES(4, '1985-10-10', 'Bob', '');
